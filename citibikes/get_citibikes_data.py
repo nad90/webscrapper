6b43d35bcf9f7ca9286b2283d1b0ac59bff5cdcc
unknown <nadiath.waidi@gmail.com>
2021-02-10 21:44:34 +0100

    citibike_first final version

--- citibikes/get_citibikes_data.py
+++ citibikes/get_citibikes_data.py
@@ -20,81 +20,65 @@ One line includes:
     Year of Birth
 """
 
-#Web scrapping libs
-import urllib.request, urllib.parse, urllib.error
+# Web scrapping libs
 import requests
 from bs4 import BeautifulSoup
 import webbrowser
 from time import sleep
-#Filtering libs
+# Filtering libs
 import re
 import fnmatch
-#File libs
+# File libs
 import shutil
 import os
 import csv
 from zipfile import ZipFile
-#Data analyses libs
+# Data analyses libs
 import pandas as pd
 
 import citibikes.dir_names
 
 print("PROCESS BEGINS")
 
-url ="https://s3.amazonaws.com/tripdata/"
-downloads_dir= citibikes.dir_names.DOWNLOADS_DIR #To replace with the local downloads dir path
-data_src_dir= citibikes.dir_names.DATA_DIR  #A refactorer data_dir #To replace with the local dir path
-zip_dir=data_src_dir+"zip/"
-csv_dir=data_src_dir+"csv/"
-final_filepath=csv_dir+"citibike_all.csv"
-"""
-data_files=[]
-response= requests.get(url)
+url = "https://s3.amazonaws.com/tripdata/"
+downloads_dir = citibikes.dir_names.DOWNLOADS_DIR  # To replace with the local downloads dir path
+data_dir = citibikes.dir_names.DATA_DIR  #To replace with the local dir path
+zip_dir = data_dir + "zip/"
+csv_dir = data_dir + "csv/"
+final_filepath = csv_dir + "citibike_all.csv"
 
-if response.status_code==200: #Transformer en erreur
-    soup=BeautifulSoup(response.text, "html.parser")
+data_files = []
+response = requests.get(url)
+
+if response.status_code == 200:  # Transformer en erreur
+    soup = BeautifulSoup(response.text, "html.parser")
 else:
     print("The URL is not responding")
     exit(0)
 
-
-#Get all the data (excluding those starting with JC).
-data_files=soup.find_all("key")
-for element in data_files:
-    file_name=element.text
+# Get all the data (excluding those starting with JC).
+data_files = soup.find_all("key")
+for element in data_files[3:5]:
+    file_name = element.text
     if re.search("^(?!JC).*zip$", file_name):
         print("DOWNLOADING ", file_name)
         webbrowser.open_new(url + file_name)
         sleep(120)
+sleep(60)
 
-
-#Move from Download folder to data folder
+# Move from Download folder to data folder
 for item in os.listdir(downloads_dir):
     print("MOVE ", item)
     shutil.move(downloads_dir + item, zip_dir)
-  """
-#Unzip the files and put it all in one file
-#Create the file
-df=pd.DataFrame(columns=["trip_duration","start_time","stop_time",
-                         "start_station_id","start_station_name","start_station_latitude","start_station_longitude",
-                         "end_station_id","end_station_name","end_station_latitude","end_station_longitude",
-                         "bike_id","user_type","birth_year","gender",
-                         "source_archive"])
 
-
-df.to_csv(final_filepath, index=False, quoting=csv.QUOTE_NONNUMERIC)
-
-for element in os.listdir(zip_dir)[:2]:
-    print("UNZIP AND WRITE ",element)
+# Unzip the files
+for element in os.listdir(zip_dir):
+    print("UNZIP ", element)
     if element.endswith(".zip"):
+        with ZipFile(zip_dir + element) as zipped_files:
+            data_file = zipped_files.open(fnmatch.filter(zipped_files.namelist(), "[0-9]*.csv")[0])
 
-        with ZipFile(zip_dir+element) as zipped_files:
-            data_file= zipped_files.open(fnmatch.filter( zipped_files.namelist(), "[0-9]*.csv")[0])
-
-
-        df_el=pd.read_csv(data_file)
-        df_el["source_archive"] = element
-        df_el.head().to_csv(final_filepath, index=False,header=False, mode="a", quoting=csv.QUOTE_ALL)
-
+        df_el = pd.read_csv(data_file)
+        df_el.to_csv(csv_dir + data_file.name, index=False, quoting=csv.QUOTE_ALL)
 
 print("PROCESS ENDS")
